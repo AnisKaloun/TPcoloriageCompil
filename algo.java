6dc4f6d1e94213b9d53229ae21bdba69db3843fd
AnisKaloun <37632125+AnisKaloun@users.noreply.github.com>
2019-10-27 13:54:22 +0100

    Update algo.java

--- algo.java
+++ algo.java
@@ -1,22 +1,28 @@
 package Algo;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Scanner;
 
 class sommet {
 	char name;
 	int nbrArrete;
+	boolean spilled;
 	public sommet(char name) {
 		this.name = name;
 		this.nbrArrete=0;
+		spilled=false;
 	}
 	public int getNbrArrete() {
 		return nbrArrete;
 	}
 	public void setNbrArrete(int nbrArrete) {
 		this.nbrArrete = nbrArrete;
+	}
+	public boolean isSpilled() {
+		return spilled;
+	}
+	public void setSpilled(boolean spilled) {
+		this.spilled = spilled;
 	}
 
 
@@ -57,11 +63,10 @@ class graph implements Cloneable {
 	}
 
 	public graph() {
-		ArrayList <sommet> Sommets=new ArrayList<sommet>();
-		ArrayList <arrete> Arretes=new ArrayList<arrete>();
 		// TODO Auto-generated constructor stub
 	}
 
+	@SuppressWarnings("unchecked")
 	public graph(graph g) {
 		Sommets=(ArrayList<sommet>) g.Sommets.clone();
 		for (int i=0;i<Sommets.size();i++)
@@ -80,9 +85,9 @@ class graph implements Cloneable {
 			System.out.printf("Sommet %d : %c nombre de voisins %d \n",i,G.Sommets.get(i).name,G.Sommets.get(i).getNbrArrete());	
 		}
 
-		System.out.println("on affiche les arretes \n");
-		System.out.println("type 0:arrete interference \n");
-		System.out.println("type 1:arrete preference \n");
+		System.out.println("on affiche les arretes ");
+		System.out.println("type 0:arrete interference");
+		System.out.println("type 1:arrete preference ");
 		for(int i=0;i<G.Arretes.size();i++)
 		{
 			System.out.printf("Arrete %d type  %d :couple (%c,%c) \n",i,G.Arretes.get(i).type,Arretes.get(i).a.name,Arretes.get(i).b.name);	
@@ -96,7 +101,7 @@ class graph implements Cloneable {
 		{
 			for (int j=0;j<this.Arretes.size();j++)
 			{
-				if(this.Arretes.get(j).a==this.Sommets.get(i) ||this.Arretes.get(j).b==this.Sommets.get(i))
+				if((this.Arretes.get(j).a==this.Sommets.get(i) ||this.Arretes.get(j).b==this.Sommets.get(i))&& (this.Arretes.get(j).type==0))
 				{
 					int x=this.Sommets.get(i).getNbrArrete();
 					this.Sommets.get(i).setNbrArrete(x+1);
@@ -108,36 +113,58 @@ class graph implements Cloneable {
 
 	void EnleverSommetEtArrete(graph G,sommet s)
 	{
-		G.Sommets.remove(s);
+
 		for(int i=0;i<G.Arretes.size();i++)
 		{
-			if(G.Arretes.get(i).a==s ||G.Arretes.get(i).b==s)
+			if((G.Arretes.get(i).a==s || G.Arretes.get(i).b==s)&&( G.Arretes.get(i).type==0))
 			{
 				if(G.Arretes.get(i).a==s)
 				{
 					sommet index=G.Arretes.get(i).b;
-					int x=this.Sommets.get(this.Sommets.indexOf(index)).getNbrArrete();
-					this.Sommets.get(this.Sommets.indexOf(index)).setNbrArrete(x-1);	
+					if(this.Sommets.indexOf(index)>=0)
+					{
+						int x=this.Sommets.get(this.Sommets.indexOf(index)).getNbrArrete();
+						this.Sommets.get(this.Sommets.indexOf(index)).setNbrArrete(x-1);
+
+					}
 				}
-				else
+				else if(G.Arretes.get(i).b==s)
 				{
 					sommet index=G.Arretes.get(i).a;
-					int x=this.Sommets.get(this.Sommets.indexOf(index)).getNbrArrete();
-					this.Sommets.get(this.Sommets.indexOf(index)).setNbrArrete(x-1);		
+					if(this.Sommets.indexOf(index)>=0)
+					{
+						int x=this.Sommets.get(this.Sommets.indexOf(index)).getNbrArrete();
+						this.Sommets.get(this.Sommets.indexOf(index)).setNbrArrete(x-1);
+
+					}
 				}
 				G.Arretes.remove(i);
-
+				i--;
 			}
 
 		}
-
+		G.Sommets.remove(s);
 	}
 
+	boolean hasPreferencedArrete(sommet s)
+	{
+		for (int i=0 ;i<Arretes.size();i++)
+		{
+			if((Arretes.get(i).a==s|| Arretes.get(i).b==s) && Arretes.get(i).type==1)
+			{
+				return true;
+			}
+
+		}
+		return false;
+
+	}
 }
 
 public class algo {
 
 	public static void main(String[] args) {
+		//graph 1
 		ArrayList <sommet> sommet= new ArrayList<sommet>();
 		sommet x=new sommet('x');
 		sommet y=new sommet('y');
@@ -166,33 +193,74 @@ public class algo {
 		System.out.print("donnez le k a colorier\n");
 		Scanner sc=new Scanner(System.in);
 		int H=sc.nextInt();
+		sc.nextLine();
 		algo g=new algo();        
-		sommet Petit=g.ChercherSommetPlusPetitDegres(G);
-		if(!g.isColoriable(Petit,H))
-		{
-			System.out.print("le graph n'ai pas k coloriable \n");	
-			sc.close();
-			return;	
-		}
 		ArrayList <sommetColore> ColoredSommet= new ArrayList<sommetColore>();
 		graph source =new graph(G);
 		source.RemplirDegres(source);
 		g.AlgoColoriage(G,H,source,ColoredSommet);
+		g.affichergraphColorier(ColoredSommet);
+
+		//graph2
+        System.out.println("\n \n \n \n le 2 eme graph");
+		ArrayList <sommet> sommet2= new ArrayList<sommet>();
+		sommet a=new sommet('a');
+		sommet b=new sommet('b');
+		sommet c=new sommet('c');
+		sommet d=new sommet('d');
+		sommet2.add(a);
+		sommet2.add(b);
+		sommet2.add(c);
+		sommet2.add(d);
+		ArrayList <arrete> arretes2=new ArrayList<arrete>();
+		arretes2.add(new arrete(0,a,b));
+		arretes2.add(new arrete(0,b,c));
+		arretes2.add(new arrete(0,c,d));
+		arretes2.add(new arrete(0,d,a));
+		graph G1 = new graph(sommet2,arretes2);
+		G1.RemplirDegres(G1);
+		G1.AfficherGraph(G1);
+		System.out.print("donnez le k a colorier\n");
+		int H1=sc.nextInt();
+		algo g1=new algo();        
+		g1.ChercherSommetPlusPetitDegres(G1);
+		ArrayList <sommetColore> ColoredSommet1= new ArrayList<sommetColore>();
+		graph source1 =new graph(G1);
+		source1.RemplirDegres(source1);
+		g1.AlgoColoriage(G1,H1,source1,ColoredSommet1);
 		sc.close();
-		for (int i=0;i<ColoredSommet.size();i++)
+		g1.affichergraphColorier(ColoredSommet1);
+
+
+	}
+
+
+
+
+	private  void affichergraphColorier(ArrayList<sommetColore> coloredSommet) {
+		for (int i=0;i<coloredSommet.size();i++)
 		{
-			System.out.println("sommet "+ColoredSommet.get(i).s.name+" couleur "+ColoredSommet.get(i).color);	
+			if(coloredSommet.get(i).s.isSpilled())
+			{
+
+
+				System.out.println("sommet - spilled "+coloredSommet.get(i).s.name+" pas de couleur");	
+
+			}
+			else
+			{
+				System.out.println("sommet "+coloredSommet.get(i).s.name+" couleur "+coloredSommet.get(i).color);	
+			}
 		}
 
 	}
-	boolean isColoriable(sommet s,int k)
+	boolean isColoriable(graph g,int k)
 	{
-		if(s.getNbrArrete()>=k)
-		{
-			return false;
-		}
-		return true;
+		if(ChercherSommetPlusPetitDegres(g).getNbrArrete()<k)
+			return true;
+		return false;
 	}
+
 	sommet ChercherSommetPlusPetitDegres(graph G)
 	{
 		sommet Petit=G.Sommets.get(0);	
@@ -209,10 +277,13 @@ public class algo {
 
 	sommet ChercherSommetPlusGrandDegres(graph G)
 	{
-		sommet Grand=G.Sommets.get(0);	
-		for(int i=0 ; i>G.Sommets.size();i++)
+
+
+		sommet Grand=G.Sommets.get(0);
+
+		for(int i=0 ; i<G.Sommets.size();i++)
 		{
-			if(G.Sommets.get(i).getNbrArrete()<=Grand.getNbrArrete())
+			if(G.Sommets.get(i).getNbrArrete()>Grand.getNbrArrete())
 			{
 				Grand=G.Sommets.get(i);
 			}
@@ -224,39 +295,38 @@ public class algo {
 	void AlgoColoriage(graph G,int k,graph source, ArrayList<sommetColore> coloredSommet)
 	{
 
-		sommet x=this.ChercherSommetPlusPetitDegres(G);
-		System.out.println("on choisis "+x.name+"\n");
-		if(!this.isColoriable(x, k))
+		sommet x=null;
+		if(!this.isColoriable(G, k))
 		{
-			System.out.println("on doit splitter içi \n");	
+			System.out.println("on doit splitter içi \n");
+			x=this.ChercherSommetPlusGrandDegres(G);
+			System.out.println("on choisis "+x.name+"\n");
+			x.setSpilled(true);
+			G.EnleverSommetEtArrete(G,x);
 		}
-		else
-		{
+		else {
 			//içi on est dans le cas ou on peut eliminer une variable
 
+			x=this.ChercherSommetPlusPetitDegres(G);
+			System.out.println("on choisis "+x.name+"\n");
 			//on as enlever le sommet
 			G.EnleverSommetEtArrete(G, x);
-			System.out.println("on affiche le nouveau graph\n");
-			graph H=new graph(G.Sommets,G.Arretes);
-			H.AfficherGraph(H); 
-			if(H.Sommets.size()>=1)
-			{
-				this.AlgoColoriage(H, k,source,coloredSommet);
-			}
-
-			//içi on est arrivé
-			//on donne les couleurs
-			//on doit recrée le graph içi
-			//on doit vérifier chaque sommet qu'on rajoute quel sont c voisins
-			//et si c voisins sont déjà dans l'array
-			//System.out.println("sommet choisis "+x.name);
-			//	System.out.println("on affiche la source");
-			//	source.AfficherGraph(source);
-			this.ColorerSommet(x,source, coloredSommet, k);
-
 
 		}
+		System.out.println("on affiche le nouveau graph\n");
+		graph H=new graph(G.Sommets,G.Arretes);
+		H.AfficherGraph(H);
+		if(H.Sommets.size()>=1)
+		{
+			this.AlgoColoriage(H,k,source,coloredSommet);
+		}
 
+		//içi on est arrivé
+		//on donne les couleurs
+		//on doit recrée le graph içi
+		//on doit vérifier chaque sommet qu'on rajoute quel sont c voisins
+		//et si c voisins sont déjà dans l'array
+		this.ColorerSommet(x,source, coloredSommet, k);
 
 	}
 
@@ -265,26 +335,18 @@ public class algo {
 		if(coloredSommet.size()==0)
 		{
 			coloredSommet.add(new sommetColore(s,1));
-			System.out.println("je suis dans le premier coloriage\n");
 			System.out.println("le sommet colorié "+coloredSommet.get(0).s.name+" couleur "+coloredSommet.get(0).color);
 		}
 		else
 		{
 
-			System.out.println("je suis dans le else\n");
-			//on initialise les couleurs
-			int voisincoloré=0;
 			int PreferedColor=-1;
 			ArrayList<Integer> color=new ArrayList<Integer>();
 			for(int i=0;i<k;i++)
 			{
 				color.add(i+1);	
 			}
-			System.out.println("les couleurs dispo :");
-			for(int i=0;i<k;i++)
-			{
-				System.out.println(""+color.get(i));	
-			}
+	
 			for(int i=0;i<coloredSommet.size();i++)
 			{
 				//içi on cherche si le s est voisin du sommet qui est déjà coloré	
@@ -293,17 +355,15 @@ public class algo {
 					//on enleve les couleurs pas disponibles
 					if(!isVoisinPreference(s,coloredSommet.get(i), g))
 					{
-					voisincoloré++;
-					System.out.println("je rentre içi je supprime de la list la couleur \n "+coloredSommet.get(i).color);
-					int x=color.indexOf(coloredSommet.get(i).color);
-					System.out.println("x :"+x);
-					if(x>=0)
-					color.remove(x);
+						int x=color.indexOf(coloredSommet.get(i).color);
+						if(x>=0)
+						{
+							color.remove(x);
+						}
 					}
 					else
 					{
-						 PreferedColor=color.indexOf(coloredSommet.get(i).color);
-						System.out.println("prefered color x :"+color.get(PreferedColor));
+						PreferedColor=color.indexOf(coloredSommet.get(i).color);
 						PreferedColor=color.get(PreferedColor);
 					}
 				}
@@ -313,28 +373,42 @@ public class algo {
 			{
 				if(PreferedColor==-1)
 				{
-				int x=color.get(0);
-				System.out.println("je colorie le sommet "+s.name+" avec la couleur "+x);
-				coloredSommet.add(new sommetColore(s,x));
+					int x=color.get(0);
+					if(!s.isSpilled())
+					System.out.println("je colorie le sommet "+s.name+" avec la couleur "+x);
+					else
+					{
+					System.out.println("on peut colorier le sommet spiller "+s.name);
+				    System.out.println("je colorie le sommet "+s.name+" avec la couleur "+x);
+				    s.setSpilled(false);
+					}
+					coloredSommet.add(new sommetColore(s,x));
 				}
 				else
 				{
-				System.out.println("je colorie le sommet "+s.name+" avec la couleur "+PreferedColor);	
-				coloredSommet.add(new sommetColore(s,PreferedColor));
-				PreferedColor=-1;
+					System.out.println("je colorie le sommet "+s.name+" avec la couleur "+PreferedColor);	
+					coloredSommet.add(new sommetColore(s,PreferedColor));
+					PreferedColor=-1;
 				}
+			}
+			else if(s.isSpilled() && color.isEmpty())
+			{
+				System.out.println("pas de couleur dispo pour  le sommet spiller "+s.name);	
+				coloredSommet.add(new sommetColore(s,-1));
+			}
+			else {
+				System.out.println("ce graph n'ai pas k coloriable");
+				System.exit(0);
 			}
 		}
 
 	}
 	private boolean isVoisin(sommet s, sommetColore sommetColore, graph g) {
 
-		System.out.println("sommet a colorier "+s.name+" sommet déjà colorier "+sommetColore.s.name);
 		for (int i=0;i<g.Arretes.size();i++)
 		{
 			if((g.Arretes.get(i).a==s && g.Arretes.get(i).b==sommetColore.s)||( g.Arretes.get(i).a==sommetColore.s && g.Arretes.get(i).b==s))
 			{
-				System.out.println("l'Arrete est presente");
 				return true;
 			}
 		}
@@ -343,15 +417,13 @@ public class algo {
 
 	private boolean isVoisinPreference(sommet s, sommetColore sommetColore, graph g) {
 
-		System.out.println("sommet a colorier "+s.name+" sommet déjà colorier "+sommetColore.s.name);
 		for (int i=0;i<g.Arretes.size();i++)
 		{
 			if((g.Arretes.get(i).a==s && g.Arretes.get(i).b==sommetColore.s)||( g.Arretes.get(i).a==sommetColore.s && g.Arretes.get(i).b==s))
 			{
 				if (g.Arretes.get(i).type==1)
 				{
-				System.out.println("j'ai trouvé l'arrete de preference");
-				return true;
+					return true;
 				}
 			}
 		}
